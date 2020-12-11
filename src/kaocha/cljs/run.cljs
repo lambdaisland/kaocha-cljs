4c9699e1d19bf83d33c6915d729bf6c03558f4a2
A Brooks <zab1000+github@gmail.com>
2020-12-11 17:30:40 -0600

    Remove version check from Clojurescript side.

--- src/kaocha/cljs/run.cljs
+++ src/kaocha/cljs/run.cljs
@@ -1,5 +1,4 @@
 ; (require 'kaocha.cljs.version-check)
 (ns kaocha.cljs.run
-  (:require cljs.test
-            kaocha.cljs.version-check)
+  (:require cljs.test)
   (:require-macros kaocha.cljs.run))
